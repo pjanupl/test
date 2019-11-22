0c25fcd91092e760617524087d493deecec0f483
pjanupl <48182321+pjanupl@users.noreply.github.com>
2019-11-22 19:15:57 +0100

    Trzecia rewizja -c

---
+++ test.py
@@ -0,0 +1,14 @@
+# (pjanu)
+# 24.06.2018 r.
+#
+#
+# Python Programming for the Absolute Beginner, 3rd Edition
+# Michael Dawson
+# page 29./ex. 2. (polish edition)
+# The program displays the name and waits for "Enter" 
+
+print("Tomasz")
+print()
+print()
+print()
+input()
